128ca987efdbae425e4e1819a5eff856c57ee308
Kamil <kamil.figon@gmail.com>
2020-04-03 21:35:43 +0200

    react router

--- 4-ecommerce-app/ecommerce-app/src/App.js
+++ 4-ecommerce-app/ecommerce-app/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Homepage from './pages/homepage/Homepage'
+import HatsPage from './pages/hats/HatsPage'
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
   );
 }
 
